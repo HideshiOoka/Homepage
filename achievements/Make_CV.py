c40ee2fe12e50ed468f5d1db0da7ce934175eed6
Hideshi_Ooka <ooka.science@gmail.com>
2023-02-15 00:13:54 +0900

    updated make CV

--- achievements/Make_CV.py
+++ achievements/Make_CV.py
@@ -1,12 +1,54 @@
 #%%
 from docx import Document
-from docx.shared import Pt
-doc = Document(r"C:\Users\Hideshi_Ooka\Research\Applications\___Archived\20200302_CV_Ooka.docx")
-doc = Document("CV_template.docx")
-pars = doc.paragraphs
+LANG = 0 # English:0 or Japanese:1
+# from docx.shared import Pt
 
-items = ["Heading", "Publications", "Presentations","Patents","Press"]
+def sort(authors):
+    sorted_authors = ""
+    for a in authors.split(" and "):
+        last,first  = a.split(", ")
+        sorted_authors += first + " " + last + ", "
+    sorted_authors = sorted_authors[:-2]        
+    return sorted_authors
+
+def make_publication_list(bib):
+    with open(bib, encoding = "UTF-8") as f:
+        bib_data = f.read().replace("\n", "")
+    entries = bib_data.split("@")[1:]
+    num_publications =len(entries)
+    corresponding_authors = str(bib_data.count("Ooka*"))
+    first_authors = str(bib_data.count("@article{Ooka"))
+    out_txt = ""
+    for i, entry in enumerate(entries):
+        authors = entry.split("author = {")[1].split("},")[0]
+        authors = sort(authors)
+        journal = entry.split("journal = {")[1].split("},   ")[0]
+        year = entry.split("year = {")[1].split("},   ")[0]
+        volume = entry.split("volume = {")[1].split("},   ")[0]
+        pages = entry.split("pages = {")[1].split("},   ")[0].replace("--","-")
+        title = entry.split("title = {")[1].split("}}")[0]
+        i+=1
+        p = doc.add_paragraph(f"{i}.  ")
+        p.add_run(authors)
+        p.add_run(" \"")
+        p.add_run(title)
+        p.add_run("\", ")
+        p.add_run(journal).italic = True
+        p.add_run(", ")
+        p.add_run(year).bold = True
+        p.add_run(", ")
+        p.add_run(pages)
+        p.add_run(".").add_break()
+    # -- in SPET
+    # # CO2, MoS2        
+
+
+
+items = ["Heading", "Publications", "Presentations","Patents","Press","Funding","Awards"]
+doc = Document("CV_template.docx")
+
+
 for item in items:
     doc.add_paragraph(item,style = "Heading 1")
-
+make_publication_list("Publications.bib")
 doc.save("CV.docx")
