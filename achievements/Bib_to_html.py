7c4d512b812425079e814e00ae2ddcbfbcf4afe7
HideshiOoka <ooka.science@gmail.com>
2023-07-14 19:39:24 +0900

    added English presentation list

--- achievements/Bib_to_html.py
+++ achievements/Bib_to_html.py
@@ -16,8 +16,6 @@ def sort(authors):
     sorted_authors = sorted_authors[:-2]        
     return sorted_authors
 
-
-
 with open("Publications.bib", encoding = "UTF-8") as f:
     bib_data = f.read().replace("\n", "")
 entries = bib_data.split("@")[1:]
@@ -27,21 +25,28 @@ first_authors = str(bib_data.count("@article{Ooka"))
 out_html = ""
 year_header = "2030"
 for entry in entries:
+    entry = entry.split("}}")[0]
     authors = entry.split("author = {")[1].split("},")[0]
     authors = sort(authors)
     journal = entry.split("journal = {")[1].split("},   ")[0]
     year = entry.split("year = {")[1].split("},   ")[0]
+    title = entry.split("title = {")[1].split("},")[0]
     volume = entry.split("volume = {")[1].split("},   ")[0]
     pages = entry.split("pages = {")[1].split("},   ")[0].replace("--","-")
-    title = entry.split("title = {")[1].split("}}")[0]
+    if "rxiv" in journal.lower():
+        doi = entry.split("doi = {")[1].split("},   ")[0]
+        status = entry.split("status = {")[1].split("},   ")[0]
+        volume = status # replace the volume and pages entry with doi and review status
+        pages = doi
+
     if year < year_header:
         year_header = year
         out_html += "\t<h2>{}</h2>\n".format(year_header)
-    out_html += "\t\t<li>{} \"{}\", <i>{}</i>, <b>{}</b>, <i>{}</i>, {}.<br>\n\n".format(authors, title, journal, year, volume, pages)
+    out_html += f"\t\t<li>{authors} \"{title}\", <i>{journal}</i>, <b>{year}</b>, <i>{volume}</i>, {pages}.<br>\n\n"
 
-out_html = out_html.replace("MoS2", "MoS<sub>2</sub>").replace("CO2", "CO<sub>2</sub>").replace("{\`e}","&egrave")
+out_html = out_html.replace("MoS$_2$", "MoS<sub>2</sub>").replace("CO$_2$", "CO<sub>2</sub>").replace("{\`e}","&egrave").replace("MnO$_2$", "MnO<sub>2</sub>")
 
-with open("../Publications_Template.html","r") as f:
+with open("publications_template.html","r") as f:
     template = f.read()
 
 out_html = template.replace("<!-- ADD_PUBLICATION_LIST_HERE-->", out_html).replace("CORRESPONDING_AUTHORS",corresponding_authors).replace("FIRST_AUTHORS", first_authors)
@@ -60,9 +65,9 @@ def translate(txt):
 out_html_jp = translate(out_html)
 
 
-with open("../Publications.html", 'w') as f:
+with open("../contents/publications_contents.html", 'w') as f:
     f.write(out_html)    
-with open("../Publications_jp.html", 'w') as f:
+with open("../contents/publications_jp_contents.html", 'w') as f:
     f.write(out_html_jp)        
 
 
