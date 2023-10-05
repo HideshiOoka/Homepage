5e2b296a20677a1267fc23c44550375d6e7d047b
HideshiOoka <ooka.science@gmail.com>
2023-10-05 19:38:19 +0900

    still need to fix publications

--- python/update_HP.py
+++ python/update_HP.py
@@ -1,16 +1,7 @@
 #%%
 import os
 import datetime
-today = datetime.date.today().strftime('%Y/%m/%d')
-page_list = ["index.html","about_me.html","research.html","publications.html","vacancies.html","blog.html","contact.html","presentations.html","funding.html","patents.html","awards.html"]
-
-with open("html_template.html","r") as f:
-    html_template = f.read()
-
 import json
-json_open = open('contents/meta_info.json', 'r')
-meta_info = json.load(json_open)
-
 def update_navbar(txt, lang):
     if lang == "":
         other_lang = "_JP"
@@ -45,17 +36,21 @@ def get_contents(page):
         with open(f"contents/construction_contents.html", "r") as f:
             contents = f.read()
     return contents
+
+today = datetime.date.today().strftime('%Y/%m/%d')
+page_list = ["index.html","about_me.html","research.html","publications.html","vacancies.html","blog.html","contact.html","presentations.html","funding.html","patents.html","awards.html"]
+with open("html_template.html","r") as f:
+    html_template = f.read()
+json_open = open('contents/meta_info.json', 'r')
+meta_info = json.load(json_open)
+
 for page in page_list:
     for lang in ["","_JP"]:
         output_html = html_template
         output_html = update_navbar(output_html, lang)
-
-
         save_file_name = page.replace(".html",f"{lang}.html")
-        
         contents = get_contents(page)
         title = make_title(page)
         output_html = output_html.replace("CONTENTS",contents).replace("PAGE_TITLE",title).replace("TODAY",today)
-
         with open(save_file_name, "w", encoding="utf-8") as f:
             f.write(output_html)
