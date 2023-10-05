b2f07033a1abf7006cbc15dd12b1b7dfb7e70760
Hideshi_Ooka <ooka.science@gmail.com>
2023-10-05 20:11:11 +0900

    pulled at home

--- python/merge_pdf.py
+++ python/merge_pdf.py
@@ -2,7 +2,7 @@
 import pypdf
 import glob
 merger = pypdf.PdfMerger()
-
+#%%
 base_dir = "C:/usr/Research/Funding/2023_Assc_Prof/20230930_Hyogo_Pref_U_Assc_Prof_Appl_Chem/"
 file_list = glob.glob(base_dir + "to_merge/*")
 print(file_list)
@@ -11,3 +11,18 @@ for i, file in enumerate(file_list):
     merger.append(file)
 merger.write(base_dir + "merged_file.pdf")
 merger.close()
+
+#%%
+
+from pypdf import PdfReader, PdfWriter
+file = "c:/Users/ohflu/Downloads/Polovtsian_Act_2.pdf"
+pdf_writer = PdfWriter()  # we want to reset this when starting a new pdf
+pdf_reader = PdfReader(file)
+pages = (138, 207)
+
+
+for idx in range(pages[0] - 1, pages[1]):
+    pdf_writer.add_page(pdf_reader.pages[idx])
+output_filename = file.replace(".pdf","extracted.pdf")
+with open(output_filename, "wb") as out:
+    pdf_writer.write(out)
