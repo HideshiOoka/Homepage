23ba027f8a22c58a523f54049aa1ade028672ae3
Hideshi_Ooka <ooka.science@gmail.com>
2024-01-26 00:26:46 +0900

    updated publication list fixed minor mistakes and added links to journal and preprints

--- python/update_publications.py
+++ python/update_publications.py
@@ -7,14 +7,11 @@ translate_dict = {"Scientific Publications":"論文",
                   "Patents":"知財・特許"}    
 def format_title(title):
     return title.replace(";",",")
-
 def format_date(date,n=8): # change format if n < 8
     date = str(date)
     formatted_date = date[:4]+"/"+date[4:6]+"/"+date[6:8]
     formatted_date = formatted_date.replace("X","")
     return formatted_date
-
-
 def format_authors(authors):
     sorted_authors = ""
     for a in authors.split(" and "):
@@ -25,46 +22,65 @@ def format_authors(authors):
             sorted_authors += a + ", "
     sorted_authors = sorted_authors[:-2]        
     return sorted_authors
-
 def translate(txt):
     for k,v in translate_dict.items():
         txt = txt.replace(k,v)
     return txt   
-
 for LANG in ["","_JP"]:
     #### Main Articles #####
-    df = pd.read_csv(f"../achievements/Publications.csv", index_col = 0) # {LANG}
-    # df = df.sort_values(by = ["type","year"], ascending = [True, False]).fillna("")
-    df = df.sort_values(by = ["year"], ascending = [False]).fillna("")
-    N = df.shape[0]
-    num_original = df[df["type"]=="original"].shape[0]
-    num_corresponding = df["author"].str.contains("Ooka*").sum() 
-    num_first = df["ID"].str.contains("Ooka").sum()
+    publications = pd.read_csv(f"../achievements/Publications.csv", index_col = 0) # {LANG}
+    publications = publications.sort_values(by = ["status"]).iloc[::-1]
+    # The 2 step sort is done to prioritize empty status (=accepted) articles in the order
+    publications = publications.sort_values(by = ["type","year"], ascending = [True, False]).fillna("")
+    original = publications[publications["type"]=="Original"]
+    review = publications[publications["type"]=="Review"]
+    editorial = publications[publications["type"]=="Editorial"]
+
+
+    num_corresponding = publications["author"].str.contains("Ooka*").sum() 
+    num_first = publications["ID"].str.contains("Ooka").sum()
     out_html = f"(Corresponding Author: {num_corresponding}, First Author: {num_first})<br>\n\n"
 
-    out_html += f"\n\t<h2>Original Articles</h2><ol>\n"
-    year_header = 2030
-
-    for i in range(N):
-        data = df.iloc[i].astype(str)
-        type,notes,status,url,preprint,bib_key,j_key,title,pages,volume,year,fullname,abbrv,journal,authors,ENTRYTYPE,ID  = data
-        year = int(year)
-        volume = volume.replace(".0","")
-        print(volume)
-        if year < year_header:
-            year_header = year
-            out_html += "\t<h2>{}</h2>\n".format(year_header)
-        authors = format_authors(authors)
-        if pages == "": # not accepted or volume undecided
-            volume = status
-            pages = url
-        
-        out_html += f"\t\t<li>{authors} \"{title}\", <i>{journal}</i>, <b>{year}</b>, <i>{volume}</i>, {pages}.<br>\n\n"
-        if i == num_original-1:
-            out_html += "\t</ol>\n\n"
-            out_html += f"<h2>Review Articles</h2><ol>\n"
-    out_html += "\t</ol>\n\n"
-
+    df_list =[original, review, editorial]
+    for i, df in enumerate(df_list):
+        if i == 0:
+            out_html += f"\n\t<h2>Original Articles</h2><ol>\n"
+        elif i == 1:
+            out_html += f"\n\t<h2>Reviews</h2><ol>\n"
+        elif i == 2:
+            out_html += f"\n\t<h2>Editorials and Perspectives</h2><ol>\n"
+        year_header = 2030
+        N = df.shape[0]
+        for j in range(N):
+            data = df.iloc[j]# .astype(str)
+            type,notes,status,url,preprint,bib_key,j_key,title,pages,volume,year,fullname,abbrv,journal,authors,ENTRYTYPE,ID  = data
+            year = int(year)
+            if year < year_header:
+                year_header = year
+                out_html += f"\t<h2>{year_header}</h2>\n"
+            if authors != "":
+                authors = format_authors(authors)
+                out_html += f"\t\t<li>{authors}"
+            if title != "":
+                out_html += f" <b>\"{title}\"</b>" 
+            if journal != "":
+                out_html += f", <i>{journal}</i>"
+            if year != "":
+                out_html += f", <b>{year}</b>"
+            if volume != "":
+                volume = int(volume)
+                out_html += f", <i>{volume}</i>"
+            if pages != "": 
+                out_html += f", {pages}"
+            if status != "":
+                out_html += f" (<i>{status}</i>)"
+            out_html += "."
+            if url != "":
+                out_html += f"    <a href=\"{url}\" target=\"_blank\">link</a>"
+            if preprint != "":
+                out_html += f"    <a href=\"{preprint}\" target=\"_blank\">preprint</a>"            
+            out_html += f"<br>\n\n" 
+        out_html += "\t</ol>\n\n"
     #### Other Articles (Non Peer Reviewed) #####
     out_html += f"<h2>Other Articles (Non Peer Reviewed)</h2><ol>\n"
     df = pd.read_csv(f"../achievements/Non_Peer_Reviewed{LANG}.csv")
@@ -88,7 +104,7 @@ for LANG in ["","_JP"]:
 
     out_html = out_html.replace("MoS$_2$", "MoS<sub>2</sub>").replace("CO$_2$", "CO<sub>2</sub>").replace("{\`e}","&egrave").replace("MnO$_2$", "MnO<sub>2</sub>")
     out_html = out_html.replace("--"," - ")
-    out_html = out_html.replace("<i></i>, ","").replace(", .", ".").replace(".</i>.",".")
+    out_html = out_html.replace("<i></i>, ","").replace(", .", ".")
 
     if LANG == "_JP":
         out_html = translate(out_html)
